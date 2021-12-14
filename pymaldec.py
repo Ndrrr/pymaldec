c187bdde1f8edefb236698396a42b9578f306651
Ndrrr <nadir999.ni95@gmail.com>
2021-12-14 22:07:06 +0400

    Update pymaldec.py

--- pymaldec.py
+++ pymaldec.py
@@ -2,6 +2,13 @@ import os
 import subprocess
 import shutil
 import pyinstxtractor
+from tkinter import *
+import tkinter.font
+import tkinter as tk
+
+
+
+
 def compile(path_from, path_to):
     try:
       path_from=os.path.expanduser(path_from)
@@ -70,11 +77,71 @@ def decompile(path_from, path_to):
     except OSError as e:
        print("Error: %s - %s." % (e.filename, e.strerror))
 
-cmd=input("Choose \"Compile\" or \"Decompile\":\n")
-a,b=input("Enter source and destination files paths:\n").split()
-if cmd=="Compile":
-  compile(a,b)
-elif cmd=="Decompile":
-  decompile(a,b)
-else:
-  print("Wrong command")
+#cmd=input("Choose \"Compile\" or \"Decompile\":\n")
+#a,b=input("Enter source and destination files paths:\n").split()
+#if cmd=="Compile":
+#  compile(a,b)
+#elif cmd=="Decompile":
+#  decompile(a,b)
+#else:
+#  print("Wrong command")
+  
+class App:
+    def __init__(self, root):
+        global path, path2,tmp
+        root.configure(background='#3f51b5')
+        root.title("Comp/Decomp")
+        width = 400
+        height = 280
+        screenwidth = root.winfo_screenwidth()
+        screenheight = root.winfo_screenheight()
+        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+        root.geometry(alignstr)
+        root.resizable(width=False, height=False)
+        ft = tk.font.Font(family='Roboto',size=10)
+        heading = Label(root, text="probably does something",bg='#3f51b5',fg='white',font=ft)
+        path = Label(root, text="Path_from", background='#3f51b5', fg='white',font=ft)
+        path2 = Label(root, text="Path_to", background='#3f51b5',fg='white',font=ft)
+        heading.grid(row=0, column=1)
+        path.grid(row=1, column=0)
+        path2.grid(row=2, column=0)
+        path = Entry(root)
+        path2 = Entry(root)
+        path.grid(row=1, column=1,ipadx="100")
+        path2.grid(row=2, column=1,ipadx="100")
+        path.bind("<Return>", path2.focus_set())
+  
+        GButton_510=tk.Button(root)
+        GButton_510["bg"] = "#757de8"
+        GButton_510["font"] = ft
+        GButton_510["fg"] = "Black"
+        GButton_510.grid(row=9, column=1)
+        GButton_510["justify"] = "center"
+        GButton_510["text"] = "Compile"
+        GButton_510.place(x=110, y=80, width=80, height=40)
+        GButton_510["command"] = self.GButton_510_command
+
+        GButton_015= Button(root)
+        GButton_015["bg"] = "#ff7961"  
+        GButton_015["font"] = ft
+        GButton_015["fg"] = "Black"
+        GButton_015.grid(row=10, column=2)
+        GButton_015["justify"] = "center"
+        GButton_015["text"] = "Decompile"
+        GButton_015.place(x=20, y=80, width=80,height=40)
+        GButton_015["command"] = self.GButton_015_command
+
+
+    def GButton_510_command(self):
+        compile(path.get(),path2.get())
+        #tmp= Label(root, text="message")
+        #tmp.place(x=20,y=150)
+    def GButton_015_command(self):
+        decompile(path.get(),path2.get())
+        
+       
+        
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
